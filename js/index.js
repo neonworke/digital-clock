e569436ec9dccf8f6520907ac353c5b9fea851bf
neonworke <amanagrawal181@gmail.com>
2021-09-19 22:09:50 +0530

    Start button added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function displayTime() {
-    const currentDate = new Date();
-    const currentTime = document.querySelector("#current-time");
+    let currentDate = new Date();
+    const currentTime = document.querySelector("#current-time");    
     currentTime.innerText = currentDate.toLocaleString("en-US", {
         hour: "numeric",
         minute: "numeric",
@@ -9,7 +9,25 @@ function displayTime() {
     });
 }
 
-const timer = setInterval(displayTime, 1000);
+function displayDate() {
+        currentDate = new Date();
+        const output = document.querySelector("#current-date");
+         let month = currentDate.getMonth() + 1;
+         let year = currentDate.getFullYear();
+         let date = currentDate.getDate();
+         
+         let current_date = `${date}-${month}-${year}`;
+         output.innerText = current_date;
+}
+
+let currdate = setInterval(displayDate)
+
+let timer = setInterval(displayTime, 1000);
 function stopClock(){
     clearInterval(timer);
 }
+
+function startClock() {
+    timer = setInterval(displayTime, 1000);
+}
+
